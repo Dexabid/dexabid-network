611f0d5383adc46763f4ad6e23449f1fc86363e3
Dexabid <ezasaamori@gmail.com>
2026-07-07 11:14:31 +0700

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import AgentBuilder from "@/components/AgentBuilder";
+import LearningDashboard from "@/components/LearningDashboard";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <AgentBuilder />
+      <LearningDashboard />
+    </>
+  );
+}
